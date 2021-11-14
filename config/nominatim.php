fd406884dd600e520ab50012d7849a0fbd1779a3
Erik Araujo <contact@erikaraujo.com>
2021-11-14 10:11:02 -0600

    Adds config options

--- config/nominatim.php
+++ config/nominatim.php
@@ -1,5 +1,25 @@
 <?php
-// config for ErikAraujo/Nominatim
-return [
 
+return [
+    'url' => env('NOMINATIM_URL', 'http://nominatim.openstreetmap.org/'),
+    'format' => 'jsonv2',
+
+    'details' => [
+        'include_address_details' => true,
+        'include_extra_tags' => true,
+        'include_name_details' => true,
+    ],
+
+    'polygon' => [
+        'include_polygon' => true,
+        'polygon_type' => 'svg',
+    ],
+
+    'email' => [
+        'include_email' => true,
+        'include_email_type' => 'default',
+
+        'default_email' => env('NOMINATIM_DEFAULT_EMAIL', ''),
+        'auth_email_field' => 'email',
+    ],
 ];
